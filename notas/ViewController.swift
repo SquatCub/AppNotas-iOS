ebba18581fdd87696a516231907fb8462d0e2e83
squatcub <brandon.alexis10@hotmail.com>
2021-04-15 00:45:24 -0500

    Insercion de nuevas notas

--- notas/ViewController.swift
+++ notas/ViewController.swift
@@ -7,21 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    // Table view methods
+class ViewController: UIViewController {
     
-    let notas = ["Ir al super", "Salir a caminar", "Hacer la tarea"]
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return notas.count
-    }
+    var defaultsBD = UserDefaults.standard
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let celda = tablaNotas.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        
-        celda.textLabel?.text = notas[indexPath.row]
-        return celda
-    }
-    
+    var notas = ["Ir al super", "Salir a caminar", "Hacer la tarea"]
     
     @IBOutlet weak var tablaNotas: UITableView!
     
@@ -30,16 +20,44 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         // Do any additional setup after loading the view.
         tablaNotas.delegate = self
         tablaNotas.dataSource = self
-    }
-    @IBAction func addNotaButton(_ sender: Any) {
-        let alerta = UIAlertController(title: "Agregar", message: "Nueva nota", preferredStyle: .alert)
-        let accion = UIAlertAction(title: "Aceptar", style: .default, handler: nil)
         
-        alerta.addAction(accion)
+        notas = defaultsBD.array(forKey: "notas") as! [String]
+    }
+    
+    @IBAction func addNotaButton(_ sender: Any) {
+        var notaTextField = UITextField()
+        
+        let alerta = UIAlertController(title: "Agregar", message: "", preferredStyle: .alert)
+        
+        alerta.addTextField { (agregarNotaAction) in
+            agregarNotaAction.placeholder = "Escribe aqui"
+            notaTextField = agregarNotaAction
+        }
+        
+        let accionAceptar = UIAlertAction(title: "Ok", style: .default) { (_) in
+            guard let nuevaNota = notaTextField.text else { return }
+            self.notas.append(nuevaNota)
+            self.tablaNotas.reloadData()
+            self.defaultsBD.set(self.notas, forKey: "notas")
+        }
+        
+        let accionCancelar = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+        alerta.addAction(accionAceptar)
+        alerta.addAction(accionCancelar)
         
         present(alerta, animated: true, completion: nil)
     }
-    
-
+}
+// Table view methods
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notas.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celda = tablaNotas.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        celda.textLabel?.text = notas[indexPath.row]
+        return celda
+    }
 }
 
